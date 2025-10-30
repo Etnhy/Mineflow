9038b2637f678fbb27b2a4e0287e16c6238f1279
Etnhy <evhenii.mahlena.dev@gmail.com>
2025-10-30 14:00:32 +0200

    Initial Commit

---
+++ Mineflow/MineflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineflowApp.swift
+//  Mineflow
+//
+//  Created by evhn on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MineflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
